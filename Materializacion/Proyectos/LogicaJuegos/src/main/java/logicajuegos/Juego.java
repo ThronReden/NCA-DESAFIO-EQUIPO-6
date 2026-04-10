af24c9f9cd37f18d57b03afce71a02c3b0c9d83f
Jorge <jsanchezsantaliestra@gmail.com>
2026-04-10 13:26:20 +0200

    crear logica 3 en raya

---
+++ Materializacion/Proyectos/LogicaJuegos/src/main/java/logicajuegos/Juego.java
@@ -0,0 +1,13 @@
+package logicajuegos;
+
+/**
+ *
+ * @author jsanchez
+ */
+abstract class Juego {
+    
+    private String jugador1;
+    private String jugador2;
+    
+    
+}
---
+++ Materializacion/Proyectos/LogicaJuegos/src/main/java/logicajuegos/Jugada3EnRaya.java
@@ -0,0 +1,33 @@
+package logicajuegos;
+
+/**
+ *
+ * @author jsanchez
+ */
+class Jugada3EnRaya {
+    
+    private int turno;
+    private int posicion;
+    private String jugador;
+    
+    public static final int ARRIBA_IZQUIERDA = 1;
+    public static final int ARRIBA = 2;
+    public static final int ARRIBA_DERECHA = 3;
+    public static final int CENTRO_IZQUIERDA = 4;
+    public static final int CENTRO = 5;
+    public static final int CENTRO_DERECHA = 6;
+    public static final int ABAJO_IZQUIERDA = 7;
+    public static final int ABAJO = 8;
+    public static final int ABAJO_DERECHA = 9;
+    
+    public Jugada3EnRaya(int turn, int pos, String j){
+        if(pos < 1 || pos > 9){
+            throw new IllegalArgumentException("Posicion no valida");
+        } else {
+            this.turno = turn;
+            this.posicion = pos;
+            this.jugador = j;
+        }
+    }
+    
+}
