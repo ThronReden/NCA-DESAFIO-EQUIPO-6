87ca666ab456ace68d29e38e9e80d728e142027c
Jorge <jorgesalvadorperez92@gmail.com>
2026-06-01 01:39:16 +0200

    Renovación y solución de bug visuales

--- Materializacion/ApartadoVisual/src/pantallas/juegos/PartidaPPTLSUI.java
+++ Materializacion/ApartadoVisual/src/pantallas/juegos/PartidaPPTLSUI.java
@@ -12,72 +12,76 @@ import logicajuegos.SupplierExcepcionesNoHayGanador;
  * @author jsanchez
  */
 public class PartidaPPTLSUI extends PartidaPPTLS {
-    
+
     public PPTLS Pantalla;
-    
-    private PartidaPPTLSUI(JugadorPPTLS j1, JugadorPPTLS j2, int nRondas, PPTLS p){
-        super(j1,j2,nRondas);
+
+    private PartidaPPTLSUI(JugadorPPTLS j1, JugadorPPTLS j2, int nRondas, PPTLS p) {
+        super(j1, j2, nRondas);
         Pantalla = p;
     }
-    
-    public static PartidaPPTLSUI crearPartida(JugadorPPTLSUI unJ, int nRondas, PPTLS p){
+
+    public static PartidaPPTLSUI crearPartida(JugadorPPTLSUI unJ, int nRondas, PPTLS p) {
         JugadorPPTLS bot = BotPPTLS.crearBot();
         JugadorPPTLS J1 = elegirQuienComienza(unJ, bot);
         JugadorPPTLS J2;
-        if(J1.equals(unJ)){
+        if (J1.equals(unJ)) {
             J2 = bot;
         } else {
             J2 = unJ;
         }
-        return new PartidaPPTLSUI(J1,J2,nRondas,p);
+        return new PartidaPPTLSUI(J1, J2, nRondas, p);
     }
-    
+
+  
     @Override
-    protected void esperarSiguienteTurno(){
-        Pantalla.continuarTurno.setEnabled(true);
-        Pantalla.continuarTurno.setVisible(true);
+    protected void esperarSiguienteTurno() {
+        Pantalla.mostrarBotonContinuar();
     }
-    
+
     @Override
-    protected void continuarTurno(){
+    protected void continuarTurno() {
         super.continuarTurno();
     }
-    
-    protected void siguienteTurno(){
+
+    protected void siguienteTurno() {
         nuevoTurno();
     }
-    
+
     @Override
     protected void mostrarJugadas(JugadaPPT jugada1, JugadaPPT jugada2) {
-       JugadaPPT jugadaPersona = jugada1.getJugador() instanceof JugadorPPTLSUI? jugada1 : jugada2;
-       JugadaPPT jugadaBot = jugadaPersona == jugada1? jugada2 : jugada1;
-       Pantalla.mostrarJugadaPersona(jugadaPersona.getGesto());
-       Pantalla.mostrarJugadaBot(jugadaBot.getGesto());
+        JugadaPPT jugadaPersona = jugada1.getJugador() instanceof JugadorPPTLSUI ? jugada1 : jugada2;
+        JugadaPPT jugadaBot = jugadaPersona == jugada1 ? jugada2 : jugada1;
+        Pantalla.mostrarJugadaPersona(jugadaPersona.getGesto());
+        Pantalla.mostrarJugadaBot(jugadaBot.getGesto());
     }
-    
+
     @Override
-    public void mostrarResultadoRonda(int resultado){
-        switch(resultado){
-            case EMPATE -> Pantalla.resultadoRondaEMPATE();
-            case GANA_J1 -> Pantalla.resultadoRondaGANA_J1();
-            case GANA_J2 -> Pantalla.resultadoRondaGANA_J2();
-            default -> throw new IllegalArgumentException();
+    public void mostrarResultadoRonda(int resultado) {
+        switch (resultado) {
+            case EMPATE ->
+                Pantalla.resultadoRondaEMPATE();
+            case GANA_J1 ->
+                Pantalla.resultadoRondaGANA_J1();
+            case GANA_J2 ->
+                Pantalla.resultadoRondaGANA_J2();
+            default ->
+                throw new IllegalArgumentException();
         }
         //Pantalla.mostrarPuntos();
     }
-    
+
     @Override
-    public void mostrarSeAcaboElTiempo(){
+    public void mostrarSeAcaboElTiempo() {
         Pantalla.pararCuentaAtras();
     }
-    
+
     @Override
-    public void mostrarGanador(){
+    public void mostrarGanador() {
         Pantalla.mostrarResultado((JugadorPPTLS) devolverGanador().orElseThrow(new SupplierExcepcionesNoHayGanador()));
     }
-    
+
     @Override
-    public void mostrarEmpate(){
+    public void mostrarEmpate() {
         Pantalla.mostrarResultado(EMPATE);
     }
 }
