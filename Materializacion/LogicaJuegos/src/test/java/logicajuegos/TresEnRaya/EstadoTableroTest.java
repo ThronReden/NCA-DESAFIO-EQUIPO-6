90196235dad8b806d1404564e15ea5d7e2293bd9
Jorge <jsanchezsantaliestra@gmail.com>
2026-06-01 09:14:33 +0200

    Reapply "Tests Partida3EnRaya y minor fixes"
    This reverts commit b55e414200c7f3f18fc91c9efb6126676f346b37.

--- Materializacion/LogicaJuegos/src/test/java/logicajuegos/TresEnRaya/EstadoTableroTest.java
+++ Materializacion/LogicaJuegos/src/test/java/logicajuegos/TresEnRaya/EstadoTableroTest.java
@@ -6,11 +6,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
